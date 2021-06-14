9abb1b4c51826b06b225b2edfcba9e5103fd5f8d
Srikar <srikarsamanchi1999@gmail.com>
2021-06-14 11:25:33 +0530

    Added : email validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,12 +10,16 @@ public class UserRegistration {
 		System.out.println("Enter the first name");
 		String firstName = sc.next();
 		// checking with regular expression
-		System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName));
+		System.out
+				.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName) ? "valid first name" : "invalid first name");
 		// taking last name
 		System.out.println("Enter last name");
 		String lastName = sc.next();
-		sc.close();
 		// checking with regular expression
-		System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName));
+		System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastName) ? "valid last name" : "invalid last name");
+		// taking email
+		System.out.println("Enter email");
+		String email = sc.next();
+		System.out.println(Pattern.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", email) ? "valid email" : "invalid email");
 	}
 }
